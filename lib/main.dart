2c1a7389ea0c959ac42e13a40e3c8109adb39109
Duc Ky <duckyngo1705@gmail.com>
2021-09-18 23:01:40 +0900

    Show irregular verbs on list

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'dart:async' show Future;
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:irregular_verbs/word.dart';
 
 void main() {
@@ -27,101 +25,124 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        // appBar: AppBar(
+        //   title: Text('Irregular Verbs List'),
+        // ),
+        body: IrregularVerbsList(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class IrregularVerbsList extends StatefulWidget{
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    return _IrregularVerbsState();
+  }
+
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-
-      final future = loadIrregularVerbs();
-      future.then((value) => print(value[0].base));
-    });
-  }
+class _IrregularVerbsState extends State<IrregularVerbsList>{
+  List _irrVerbList = <Word>[];    // Create a array list of type Word
+  final _savedList = <String>{};      // Create a Set of type String
+  final _biggerFont = const TextStyle(fontSize: 18);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final future = loadIrregularVerbs();
+    future.then((value) => {
+      setState((){
+        _irrVerbList = value;
+      })
+    });
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("All verbs"),
+        actions: [IconButton(onPressed: _pushFavorite, icon: Icon(Icons.list_rounded))],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: _buildWordList(),
+    );
+  }
+
+  Widget _buildWordList(){
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (BuildContext _context, int i){
+        if (i.isOdd){
+          return Divider();
+        }
+
+        final int index = i~/2;
+
+        return _buildRow(_irrVerbList[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(Word verb){
+    final alreadySaved = _savedList.contains(verb.base);
+
+    return ListTile(
+      title: Text(
+        verb.base,
+        style: _biggerFont,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: (){      // Note: Flutter reactive calling setState
+        setState((){    // => triggers build to update UI
+          if (alreadySaved) {
+            _savedList.remove(verb.base);
+          } else{
+            _savedList.add(verb.base);
+          }
+        });
+      },
+    );
+  }
+
+  void _pushFavorite() {
+    Navigator.of(context).push(
+
+        MaterialPageRoute(builder: (BuildContext context) {
+          final tiles = _savedList.map(
+                  (String word){
+                return ListTile(
+                  title: Text(
+                    word,
+                    style: _biggerFont,
+                  ),
+                );
+              }
+          );
+
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(tiles: tiles, context: context).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+                title: Text('Favorite Words')
+            ),
+            // Note: ListView can created by ListView.builder...
+            // Or create by ListView(children: list..)
+            body: ListView(children: divided,),
+          );
+
+        })
     );
   }
 
 
+  /**
+   * Load Irregular Verb list from json file
+   */
   Future<List<Word>> loadIrregularVerbs() async {
     String data = await DefaultAssetBundle.of(context).loadString("assets/irregular_verbs.json");
     final jsonResult = jsonDecode(data); //latest Dart
